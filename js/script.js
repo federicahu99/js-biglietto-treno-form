4bb245cab8a9d75fbbb52c1fe748d0e6b05e81c3
feddericahu99 <federica.hu339@gmail.com>
2022-06-22 17:10:53 +0200

    submit

--- js/script.js
+++ js/script.js
@@ -3,27 +3,18 @@
 2-Indico all'utente se ho applicato sconto e prezzo finale
 3-Creo un biglietto digitale con dati e prezzo. 
 */
+/* Start point */
+let btn = document.getElementById('bnt-submit');
+let fullName = document.getElementById('full-name');
+let trip = document.getElementById('trip-km');
 
 /* Submit Button */
-const btn = document.getElementById('bnt-submit')
-btn.addEventListener('click', function() {
 
+btn.addEventListener('click', function() {
+    console.log(fullName.value);
+    console.log(trip.value);
 })
 
 
-/* Starting Price (without Discounts) */
-const NormalPrice = (kmTrip * 0.21).toFixed(2);
-console.log(NormalPrice);
 
-/* Disccounts */
 
-if ( ageUser < 18 ) {
-    let Under18 = ( ( kmTrip * 0.21 ) * 0.8).toFixed(2);
-    console.log(Under18);
-    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + Under18.toFixed(2);
-    
-} else if ( 65 < ageUser ) {
-    let over65 = ( ( kmTrip * 0.21 ) * 0.6).toFixed(2);
-    console.log(over65);
-    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + over65.toFixed(2);
-}
