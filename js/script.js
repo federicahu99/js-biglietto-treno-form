1fa5263a7217e582a973185bd84290da03bb01ab
feddericahu99 <federica.hu339@gmail.com>
2022-06-22 16:46:58 +0200

    added eventlistener on javascript

--- js/script.js
+++ js/script.js
@@ -4,14 +4,32 @@
 3-Creo un biglietto digitale con dati e prezzo. 
 */
 
+
+/* Ask for fullname */
+let fullNameUser = prompt('What is it your fullname?').value
+console.log(fullName);
+document.getElementById('full-name').innerText = '' + fullName;
+
+
+
+/* Ask for km he has to travel */
+let age = prompt('What is it your fullname?').value
+console.log(fullName);
+
+
 // Starting information
 
-const fullName = document.getElementById('name').value;
-console.log(fullName);
 const kmTrip = document.getElementById('trip-km').value;
 console.log(kmTrip);
 const ageUser = document.getElementById('age').value;
 console.log(ageUser);
+
+/* Submit Button */
+const btn = document.getElementById('bnt-submit')
+btn.addEventListener('click', function() {
+
+})
+
 
 /* Starting Price (without Discounts) */
 const NormalPrice = (kmTrip * 0.21).toFixed(2);
@@ -22,15 +40,10 @@ console.log(NormalPrice);
 if ( ageUser < 18 ) {
     let Under18 = ( ( kmTrip * 0.21 ) * 0.8).toFixed(2);
     console.log(Under18);
-    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoGiovani.toFixed(2);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + Under18.toFixed(2);
     
 } else if ( 65 < ageUser ) {
     let over65 = ( ( kmTrip * 0.21 ) * 0.6).toFixed(2);
     console.log(over65);
-    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoOver.toFixed(2);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + over65.toFixed(2);
 }
-
-/*
-
-document.getElementById('prezzo-non-scontato').innerText = '€' + costoIniziale;
-*/
