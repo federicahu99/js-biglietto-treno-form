bd0f55520a697d4046f20f04841be7e121144522
feddericahu99 <federica.hu339@gmail.com>
2022-06-22 16:05:27 +0200

    html select + js disccounts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 /*
-1-Chiedo nome e cognome
-2-Chiedo km da percorrere
-3-Chiedo l'età
+1-Se > di 65 anni sconto 40, se 18 < sconto del 20
+2-Indico all'utente se ho applicato sconto e prezzo finale
+3-Creo un biglietto digitale con dati e prezzo. 
+*/
 
+// Starting information
+
+const fullName = document.getElementById('name').value;
+console.log(fullName);
+const kmTrip = document.getElementById('trip-km').value;
+console.log(kmTrip);
+const ageUser = document.getElementById('age').value;
+console.log(ageUser);
+
+/* Starting Price (without Discounts) */
+const NormalPrice = (kmTrip * 0.21).toFixed(2);
+console.log(NormalPrice);
+
+/* Disccounts */
+
+if ( ageUser < 18 ) {
+    let Under18 = ( ( kmTrip * 0.21 ) * 0.8).toFixed(2);
+    console.log(Under18);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoGiovani.toFixed(2);
+    
+} else if ( 65 < ageUser ) {
+    let over65 = ( ( kmTrip * 0.21 ) * 0.6).toFixed(2);
+    console.log(over65);
+    document.getElementById('nuovo-prezzo').innerText = 'Nuovo prezzo scontato:' + '  ' + '€' + prezzoOver.toFixed(2);
+}
+
+/*
+
+document.getElementById('prezzo-non-scontato').innerText = '€' + costoIniziale;
 */
