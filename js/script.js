bf33cd6ebfb37a9d5bd82c6e98d1173592bb89f8
feddericahu99 <federica.hu339@gmail.com>
2022-06-28 19:12:04 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -11,55 +11,66 @@ const distance = document.getElementById('distance');
 const age = document.getElementById('age');
 const submit = document.getElementById('submit');
 const cancel = document.getElementById('cancel');
+//dati ticket form
 
 // Prendo i campi da riempire
-const fullNameTicket = document.getElementById('fullname-passenger');
-const distanceTicket = document.getElementById('km-trip');
-let price = document.getElementById('initial-price');
-let discounted = document.getElementById('discounter-price');
+let fullNameTicket;
+let distanceTicket;
+let price;
+let discounted;
+//variabili appoggio per anteprima
 
 // Faccio funzionare il btn submit
 submit.addEventListener('click', function() {
     let fullNameAdd = fullName.value;
     let distanceAdd = distance.value;
+    let discountedPrice
     let ageAdd = age.value;
     console.log( fullNameAdd, distanceAdd, ageAdd ) 
+//prendo variabili da form
     
-    /* Calcolo prezzo */
+/* Calcolo prezzo */
     let initialPrice = distanceAdd * 0.21;
-    console.log(initialPrice.toFixed(2))
+    console.log(initialPrice.toFixed(2));
 
     /* Mostro biglietto */
     let infoArea = document.getElementById('_info-area')
     infoArea.classList.remove('d-none');
-    
-    /* Vedo se rientra tra i sconti */
+    fullNameTicket = document.getElementById('fullname-passenger');
+    distanceTicket = document.getElementById('km-trip');
+    price = document.getElementById('initial-price');
+    discounted = document.getElementById('discounted-price');
+    console.log(discounted);
+
+    /* Vedo se l'età rientra tra i sconti */
     if (ageAdd == 'u' ) {
-        let discountedPrice = initialPrice * 0.8;
+        discountedPrice = initialPrice * 0.8;
         console.log(discountedPrice.toFixed(2));
     } else if (ageAdd == 'o' ) {
-        let discountedPrice = initialPrice * 0.6;
+        discountedPrice = initialPrice * 0.6;
         console.log(discountedPrice.toFixed(2));
     } else {
-        let discountedPrice = 0;
+        discountedPrice = 0;
+        console.log(initialPrice)
     }
 
     /* Inserisco dati nell'anteprima */
     fullNameTicket.append(fullNameAdd);
     distanceTicket.append(distanceAdd);
     price.append(initialPrice);
-    discounted.append(discountedPrice);
+    discounted.append(discountedPrice.toFixed(2));
+
+    /* Btn cancel */
+    cancel.addEventListener('click', function() {
+        fullNameAdd= '';
+        price.innerHTML= fullName;
+        distanceAdd= '';
+        distanceAdd.innerTHTML= distance;
+        age.value = '';
+        age.value= age;
+        infoArea = document.getElementById('_info-area');
+        infoArea.classList.add('d-none');
+    })
 })
 
-/* Btn cancel */
-cancel.addEventListener('click', function() {
-    let infoArea = document.getElementById('_info-area')
-    infoArea.classList.add('d-none');
-    fullName.value = '';
-    distance.value = '';
-    age.value = '';
-})
 
-/* Da revisionare:
-Bottone cancel non sovrascrive valori precedenti (anche se non settati) nel Dom.
-*/
